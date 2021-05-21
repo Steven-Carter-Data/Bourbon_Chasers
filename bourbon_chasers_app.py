e3972e35e6ce003c2e111d461fc6b1faeb0657cd
Carter <612721@bah.com>
2021-05-21 10:32:43 -0400

    latest. adding af race pics

--- bourbon_chasers_app.py
+++ bourbon_chasers_app.py
@@ -152,7 +152,7 @@ elif select == "Pictures":
     col1.image(image= 'tri3.png', use_column_width=True)
     col1.image(image= 'af_logo.png', use_column_width=True)
     col2.image(image= 'af1.jpg', use_column_width=True)
-    col1.video(video= 'toddrun.mp4', start_time=0)
+    col1.video('toddrun.mp4', start_time=0)
     st.subheader('More pics on the way!!')
 
 elif select == "Inspiration":
