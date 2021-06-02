bbd4a637e198986d96b5d02741b7303e0f17753e
Carter <612721@bah.com>
2021-06-02 10:10:40 -0400

    latest. configure averages and more stats

--- bourbon_chasers_app.py
+++ bourbon_chasers_app.py
@@ -159,12 +159,13 @@ elif select == "Weekly & Overall Leaderboard":
     col1.write('#### Weekly Points Average Among the Bourbon Chasers')
     col1.write(week_df.mean())
 
-    fig = px.line(
+    fig = px.scatter(
         data_frame = week_df,
         y = week_df.mean(),
         labels = {
             "y" : "Weekly Average"
-        }
+        },
+        title = "Bourbon Chaser Points -- Weekly Average"
     )
     col2.plotly_chart(fig)
 
